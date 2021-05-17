eecf3caaff11086b09ea557f21a2af04be0582e5
barkinak <barkinak@outlook.com>
2021-05-17 23:59:12 +0300

    section 8

--- api/Extensions/ApplicationServiceExtensions.cs
+++ api/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,8 @@
 using api.Data;
+using api.Helpers;
 using api.Interfaces;
 using api.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,6 +14,8 @@ namespace api.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(config.GetConnectionString("DefaultConnection"));
