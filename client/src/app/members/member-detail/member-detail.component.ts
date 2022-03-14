8cc1d29b42d819aae978da6f35ddd4994bd49419
barkinak <barkinak@outlook.com>
2022-03-14 19:28:59 +0300

    section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from 'src/app/_models/member';
+import { Photo } from 'src/app/_models/photo';
+import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
+  slides: { image: string; text?: string }[] = [];
+  activeSlideIndex = 0;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member => {
+      this.member = member;
+      this.addPictures(member.photos);
+    })
+  }
+
+  addPictures(photos: Photo[]) {
+    console.log(photos);
+    for (let i = 0; i < photos.length; i++) {
+      this.slides.push({
+        image: `${photos[i].url}`
+      });
+    }
+  }
+  
 }
